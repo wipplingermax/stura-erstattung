c3ddf32c46316398c0bcd3bd2013d0a56350c4c9
Max Wipplinger <wipplinger.max@gmail.com>
2022-11-12 05:11:53 +0100

    init DB

--- server/main.go
+++ server/main.go
@@ -2,10 +2,49 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	// "net/http"
 	// "github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	models "server/models"
 )
 
-func main(){
-	fmt.Println("Server started")
+var DB *gorm.DB
+
+func initDB() (*gorm.DB, error) {
+
+	var user string = os.Getenv("DB_USER")
+	var password string = os.Getenv("DB_PASSWORD")
+	var host string = os.Getenv("DB_HOST")
+	var port string = os.Getenv("DB_PORT")
+	var dbname string = os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
+
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+}
+
+func main() {
+
+	if os.Getenv("DEVELOPMENT") == "true" {
+
+		err := godotenv.Load("./config/.env")
+		if err != nil {
+			fmt.Println("Error loading .env file")
+		}
+
+	}
+
+	db, err := initDB()
+	if err != nil {
+		fmt.Printf("Error connecting to DB: %s", err)
+	}
+
+	if os.Getenv("MIGRATE") == "true" {
+		db.AutoMigrate(&models.Request{})
+	}
 }
---
+++ server/models/request.go
@@ -0,0 +1,13 @@
+package models
+
+type Request struct {
+	FirstName           string
+	LastName            string
+	MatriculationNumber string
+	UniID               string
+	Email               string
+	Phone               string
+	IBAN                string
+	BIC                 string
+	AccountOwner        string
+}
