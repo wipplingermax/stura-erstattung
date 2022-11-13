725b692215cbde6c378b34d43e8d50555ce93fbd
Max Wipplinger <wipplinger.max@gmail.com>
2022-11-13 19:36:18 +0100

    GORM logging to seperate File

--- server/main.go
+++ server/main.go
@@ -18,8 +18,19 @@ import (
 
 func initLogToFile() {
 	// If the file doesn't exist, create it or append to the file
+	var path string = "/var/log"
+	var filename string = "stura-erstattung.log"
+
+	if os.Getenv("LOG_PATH") != "" {
+		path = os.Getenv("LOG_PATH")
+	}
+
+	if os.Getenv("LOG_FILE") != "" {
+		filename = os.Getenv("LOG_FILE")
+	}
+
 	file, err := os.OpenFile(
-		fmt.Sprintf("%s/%s", os.Getenv("LOGGING_PATH"), os.Getenv("LOGGING_FILE")),
+		fmt.Sprintf("%s/%s", path, filename),
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
 		0666)
 
@@ -42,11 +53,14 @@ func initDB() (*gorm.DB, error) {
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
 
-	// configure Logging
-	var gormConfig gorm.Config
-	var gormLogLevel logger.LogLevel
+	var gormConfig gorm.Config = gorm.Config{}
 
+	// configure Logging
 	if os.Getenv("GORM_LOGGING") == "true" {
+
+		// configure loglevel - default: Info
+		var gormLogLevel logger.LogLevel
+
 		switch os.Getenv("GORM_LOGLEVEL") {
 		case "Silent":
 			gormLogLevel = logger.Silent
@@ -56,10 +70,48 @@ func initDB() (*gorm.DB, error) {
 			gormLogLevel = logger.Warn
 		case "Info":
 			gormLogLevel = logger.Info
+		default:
+			gormLogLevel = logger.Info
 		}
-		gormConfig = gorm.Config{Logger: logger.Default.LogMode(gormLogLevel)}
-	} else {
-		gormConfig = gorm.Config{}
+
+		// configure file logging - default path: /var/log/stura-erstattung.log
+		gormLogPaths := io.Writer(os.Stdout)
+
+		if os.Getenv("GORM_LOG_TO_FILE") == "true" {
+
+			var path string = "/var/log"
+			var filename string = "stura-erstattung-db.log"
+
+			if os.Getenv("GORM_LOG_PATH") != "" {
+				path = os.Getenv("GORM_LOG_PATH")
+			}
+
+			if os.Getenv("GORM_LOG_FILE") != "" {
+				filename = os.Getenv("GORM_LOG_FILE")
+			}
+
+			file, err := os.OpenFile(
+				fmt.Sprintf("%s/%s", path, filename),
+				os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+				0666)
+
+			if err != nil {
+				log.Printf("Failed to Log into File: %s", err)
+			}
+
+			gormLogPaths = io.MultiWriter(os.Stdout, file)
+		}
+
+		gormLogger := logger.New(
+			log.New(gormLogPaths, "\r\n", log.LstdFlags), // io writer
+			logger.Config{
+				LogLevel:                  gormLogLevel, // Log level
+				IgnoreRecordNotFoundError: true,         // Ignore ErrRecordNotFound error for logger
+				Colorful:                  false,        // Disable color
+			},
+		)
+
+		gormConfig = gorm.Config{Logger: gormLogger}
 	}
 
 	return gorm.Open(postgres.Open(dsn), &gormConfig)
@@ -71,7 +123,7 @@ func main() {
 	if os.Getenv("DEVELOPMENT") == "true" {
 		err := godotenv.Load("./config/.env")
 		if err != nil {
-			fmt.Printf("Error while loading .env file for DEVELOPMENT: %s", err)
+			fmt.Println("Error while loading .env file for DEVELOPMENT")
 		}
 	}
 
@@ -84,7 +136,7 @@ func main() {
 	db, err := initDB()
 
 	if err != nil {
-		log.Fatalf("Error while initializing DB: %s", err)
+		log.Fatal("Error while initializing DB")
 	}
 
 	// optional: migrate database
