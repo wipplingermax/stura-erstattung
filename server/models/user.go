c91bd74189b544c978f8ae804f0a619a99695c60
Max Wipplinger <wipplinger.max@gmail.com>
2022-12-12 01:02:31 +0100

    added Role to User

--- server/models/user.go
+++ server/models/user.go
@@ -5,6 +5,7 @@ import loggable "github.com/daqingshu/gorm-loggable"
 type User struct {
 	UUIDPkey
 	loggable.LoggableModel
+	Role     string `gorm:"not null"`
 	Name     string `gorm:"not null"`
-	Password string `gorm:"not null"`
+	Password string
 }
